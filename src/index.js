33e4d50e63ab2809c7162041925c1e6ceeecc981
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-12-17 19:04:26 -0600

    first treehouse react basics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const title = <h1>My first react element!</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const desc = <p> just learned how to create a react element and render it to the DOM.</p>;
+
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc
+);
+
+console.log(title);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
+
+
