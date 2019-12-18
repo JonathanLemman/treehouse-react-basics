3093ff612511125daa64b02d5a0337c9084f03b5
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-12-17 19:18:01 -0600

    finished React Elements basics

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
 
-const title = <h1>My first react element!</h1>;
+const desc = 'just learned how to create a react element and render it to the DOM.';
+const myTitleID = 'main-title';
+const name = 'Lemon';
 
-const desc = <p> just learned how to create a react element and render it to the DOM.</p>;
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+        <h1 id={myTitleID}>{name}'s First React Element</h1>
+        <p>{desc}</p>
+    </header>
 );
 
-console.log(title);
 
 ReactDOM.render(
     header,
